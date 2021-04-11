120eee95e73a6a4e5794d0e99848ac694bb2ac49
Mohammed <Moeldowy@iti.gov.eg>
2021-04-11 10:37:20 +0200

    Daye 2 Break1

--- Day2/index.php
+++ Day2/index.php
@@ -1,1 +1,43 @@
 <?php
+//declare indexed array
+//$names=array("Mahmoud","Ali","Khaled");
+//print_r($names)."<br>";
+$mobiles=["Samsung","iPhone","Opoo"];
+echo $mobiles[1]."<hr>";
+$count=count($mobiles);
+//for($i=0;$i<$count;$i++){
+//    echo $mobiles[$i]."<br>";
+//}
+$i=0;
+//while($i<$count){
+//    echo $mobiles[$i];
+//    $i++;
+//}
+//do{
+//    echo $mobiles[$i];
+//    $i++;
+//}while($i<$count);
+
+echo gettype($mobiles);
+echo"<pre>";
+//print_r($mobiles);
+echo"</pre>";
+echo"<hr>";
+//declare Associative array
+$family=[
+    "Father"=>"Ali",
+    "Son"=>"Ashraf",
+];
+echo $family["Father"]."<br>";
+print_r($family);
+echo"<hr>";
+//Multi dimension array
+$SWDF=[
+    "Course1"=>"Softskills",
+    "programming"=>["A"=>"OOP","OS","PHP"],
+    "Project"=>"Website Project"
+];
+echo gettype($SWDF)."<br>";
+echo $SWDF["programming"][0]."<br>";
+echo $SWDF["programming"]["A"]."<br>";
+print_r($SWDF);

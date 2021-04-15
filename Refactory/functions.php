9d9cf124b0286918126e0a7faf141c150f1f4699
Mohammed <Moeldowy@iti.gov.eg>
2021-04-15 10:51:47 +0200

    Day4 upload file;

---
+++ Refactory/functions.php
@@ -0,0 +1,9 @@
+<?php
+//This function calc or sum two numbers
+function sumTwoNums($x,$y){
+    return $x+$y;
+}
+//This Function return welcome message
+function welcome($username){
+    return "Welcome my dear {$username}";
+}
---
+++ Refactory/index.php
@@ -0,0 +1,4 @@
+<?php
+require "functions.php";
+$mobiles=["Samsung","Iphone","xaomi"];
+require "index.view.php";
